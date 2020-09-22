240e7af64b235e7685f91486d45336630e68938f
Vladislav Mikitich <v.mikitich@affise.com>
2020-09-23 00:46:19 +0300

    Added GasExcludingSetUpContextDecorator
    CreatePDVMsg released from gas

--- app/app.go
+++ app/app.go
@@ -6,9 +6,6 @@ import (
 	"os"
 
 	"github.com/Decentr-net/cerberus/pkg/api"
-	"github.com/Decentr-net/decentr/x/pdv"
-	"github.com/Decentr-net/decentr/x/profile"
-	"github.com/Decentr-net/decentr/x/token"
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/simapp"
@@ -28,6 +25,10 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 	tmos "github.com/tendermint/tendermint/libs/os"
 	dbm "github.com/tendermint/tm-db"
+
+	"github.com/Decentr-net/decentr/x/pdv"
+	"github.com/Decentr-net/decentr/x/profile"
+	"github.com/Decentr-net/decentr/x/token"
 )
 
 const appName = "decentr"
@@ -308,13 +309,7 @@ func NewDecentrApp(
 	app.SetEndBlocker(app.EndBlocker)
 
 	// The AnteHandler handles signature verification and transaction pre-processing
-	app.SetAnteHandler(
-		auth.NewAnteHandler(
-			app.accountKeeper,
-			app.supplyKeeper,
-			auth.DefaultSigVerificationGasConsumer,
-		),
-	)
+	app.SetAnteHandler(NewAnteHandler(app.accountKeeper, app.supplyKeeper))
 
 	// initialize stores
 	app.MountKVStores(keys)
---
+++ app/setup.go
@@ -0,0 +1,90 @@
+package app
+
+import (
+	"reflect"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	"github.com/cosmos/cosmos-sdk/x/supply"
+
+	"github.com/Decentr-net/decentr/x/pdv"
+)
+
+func NewAnteHandler(ak auth.AccountKeeper, sk supply.Keeper) sdk.AnteHandler {
+	return sdk.ChainAnteDecorators(
+		NewGasExcludingSetUpContextDecorator(pdv.MsgCreatePDV{}),
+		ante.NewMempoolFeeDecorator(),
+		ante.NewValidateBasicDecorator(),
+		ante.NewValidateMemoDecorator(ak),
+		ante.NewConsumeGasForTxSizeDecorator(ak),
+		ante.NewSetPubKeyDecorator(ak), // SetPubKeyDecorator must be called before all signature verification decorators
+		ante.NewValidateSigCountDecorator(ak),
+		ante.NewDeductFeeDecorator(ak, sk),
+		ante.NewSigGasConsumeDecorator(ak, auth.DefaultSigVerificationGasConsumer),
+		ante.NewSigVerificationDecorator(ak),
+		ante.NewIncrementSequenceDecorator(ak), // innermost AnteDecorator
+	)
+}
+
+type GasExcludingSetUpContextDecorator struct {
+	Exclude []reflect.Type
+}
+
+func NewGasExcludingSetUpContextDecorator(exclude ...interface{}) GasExcludingSetUpContextDecorator {
+	e := make([]reflect.Type, len(exclude))
+
+	for i, v := range exclude {
+		e[i] = reflect.TypeOf(v)
+	}
+
+	return GasExcludingSetUpContextDecorator{
+		Exclude: e,
+	}
+}
+
+func (sud GasExcludingSetUpContextDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
+	freeFromGas := true
+
+check:
+	for _, v := range tx.GetMsgs() {
+		for _, e := range sud.Exclude {
+			if reflect.TypeOf(v).String() != e.String() {
+				freeFromGas = false
+				break check
+			}
+		}
+	}
+
+	if !freeFromGas {
+		return ante.NewSetUpContextDecorator().AnteHandle(ctx, tx, simulate, next)
+	}
+
+	return next(ctx.WithGasMeter(freeGasMeter{}).WithMinGasPrices(nil), tx, simulate)
+}
+
+type freeGasMeter struct {
+}
+
+func (g freeGasMeter) GasConsumed() sdk.Gas {
+	return 0
+}
+
+func (g freeGasMeter) GasConsumedToLimit() sdk.Gas {
+	return 0
+}
+
+func (g freeGasMeter) Limit() sdk.Gas {
+	return 0
+}
+
+func (g freeGasMeter) ConsumeGas(_ sdk.Gas, _ string) {
+}
+
+func (g freeGasMeter) IsPastLimit() bool {
+	return false
+}
+
+func (g freeGasMeter) IsOutOfGas() bool {
+	return false
+}
