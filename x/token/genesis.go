0ae9e237de9eb27a2b9125788a663e28a484d42c
Andrei Shneider <andrei.shneider@scorum.com>
2021-01-25 18:49:19 +0300

    Export PDV tokens in genesis

--- x/token/genesis.go
+++ x/token/genesis.go
@@ -5,19 +5,44 @@ import (
 )
 
 type GenesisState struct {
+	Balances map[string]sdk.Int `json:"balances"`
 }
 
 func ValidateGenesis(data GenesisState) error {
+	for account := range data.Balances {
+		if _, err := sdk.AccAddressFromBech32(account); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func DefaultGenesisState() GenesisState {
-	return GenesisState{}
+	return GenesisState{
+		Balances: make(map[string]sdk.Int),
+	}
 }
 
 func InitGenesis(ctx sdk.Context, keeper Keeper, data GenesisState) {
+	for account, balance := range data.Balances {
+		addr, _ := sdk.AccAddressFromBech32(account)
+		keeper.SetBalance(ctx, addr, balance)
+	}
 }
 
-func ExportGenesis(ctx sdk.Context, k Keeper) GenesisState {
-	return GenesisState{}
+func ExportGenesis(ctx sdk.Context, keeper Keeper) GenesisState {
+	out := make(map[string]sdk.Int)
+
+	it := keeper.GetBalanceIterator(ctx)
+	defer it.Close()
+
+	for ; it.Valid(); it.Next() {
+		amount := keeper.GetBalance(ctx, it.Key())
+		owner := sdk.AccAddress(it.Key())
+		out[owner.String()] = amount
+	}
+
+	return GenesisState{
+		Balances: out,
+	}
 }
