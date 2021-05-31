2f5564279604ccc85a63ede9c1284396e8277a96
Andrei Shneider <megaherz@bk.ru>
2021-06-01 00:09:41 +0300

    Fix distribute gas

--- app/ante/fixedgastx.go
+++ app/ante/fixedgastx.go
@@ -3,6 +3,7 @@ package ante
 import (
 	"github.com/Decentr-net/decentr/x/community"
 	"github.com/Decentr-net/decentr/x/operations"
+	"github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -11,26 +12,48 @@ type FixedGasTxDecorator struct {
 }
 
 func NewFixedGasTxDecorator(pk operations.Keeper, ck community.Keeper) FixedGasTxDecorator {
+	// suppress GasReadCostFlatDesc since call to any GetFixedGasParams lead to panic
+	suppressOutOfGasPanic := func() {
+		if r := recover(); r != nil {
+			switch rType := r.(type) {
+			case sdk.ErrorOutOfGas:
+				if rType.Descriptor == types.GasReadCostFlatDesc {
+					return
+				}
+			default:
+				// some other panic, rethrow
+				panic(r)
+			}
+		}
+	}
+
 	config := map[string]func(ctx sdk.Context) sdk.Gas{
 		operations.MsgResetAccount{}.Type(): func(ctx sdk.Context) sdk.Gas {
+			defer suppressOutOfGasPanic()
 			return pk.GetFixedGasParams(ctx).ResetAccount
 		},
 		operations.MsgDistributeRewards{}.Type(): func(ctx sdk.Context) sdk.Gas {
+			defer suppressOutOfGasPanic()
 			return pk.GetFixedGasParams(ctx).DistributeRewards
 		},
 		community.MsgCreatePost{}.Type(): func(ctx sdk.Context) sdk.Gas {
+			defer suppressOutOfGasPanic()
 			return ck.GetFixedGasParams(ctx).CreatePost
 		},
 		community.MsgDeletePost{}.Type(): func(ctx sdk.Context) sdk.Gas {
+			defer suppressOutOfGasPanic()
 			return ck.GetFixedGasParams(ctx).DeletePost
 		},
 		community.MsgSetLike{}.Type(): func(ctx sdk.Context) sdk.Gas {
+			defer suppressOutOfGasPanic()
 			return ck.GetFixedGasParams(ctx).SetLike
 		},
 		community.MsgFollow{}.Type(): func(ctx sdk.Context) sdk.Gas {
+			defer suppressOutOfGasPanic()
 			return ck.GetFixedGasParams(ctx).Follow
 		},
 		community.MsgUnfollow{}.Type(): func(ctx sdk.Context) sdk.Gas {
+			defer suppressOutOfGasPanic()
 			return ck.GetFixedGasParams(ctx).Unfollow
 		},
 	}
@@ -45,7 +68,7 @@ func (fgm FixedGasTxDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate b
 		if fixedGas, ok := fgm.config[msg.Type()]; ok {
 			limit := ctx.GasMeter().Limit()
 			consumed := fixedGas(ctx)
-			return ctx.WithGasMeter(NewFixedGasMeter(consumed, limit)), nil
+			return next(ctx.WithGasMeter(NewFixedGasMeter(consumed, limit)), tx, simulate)
 		}
 	}
 
--- x/operations/client/cli/tx.go
+++ x/operations/client/cli/tx.go
@@ -2,27 +2,22 @@ package cli
 
 import (
 	"bufio"
-	"encoding/hex"
 	"fmt"
-	"io/ioutil"
-
 	"github.com/Decentr-net/decentr/x/operations/types"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/client/keys"
 	"github.com/cosmos/cosmos-sdk/codec"
-	keyring "github.com/cosmos/cosmos-sdk/crypto/keys"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"strconv"
 )
 
 // GetTxCmd returns the transaction commands for this module
 func GetTxCmd(storeKey string, cdc *codec.Codec) *cobra.Command {
-	pdvTxCmd := &cobra.Command{
+	operationsTxCmd := &cobra.Command{
 		Use:                        types.ModuleName,
 		Aliases:                    []string{"op"},
 		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
@@ -31,46 +26,50 @@ func GetTxCmd(storeKey string, cdc *codec.Codec) *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	pdvTxCmd.AddCommand(flags.PostCommands(
-		GetCmdSignPDV(cdc),
+	operationsTxCmd.AddCommand(flags.PostCommands(
 		GetCmdResetAccount(cdc),
+		GetCmdDistributeReward(cdc),
 	)...)
 
-	return pdvTxCmd
+	return operationsTxCmd
 }
 
-func GetCmdSignPDV(cdc *codec.Codec) *cobra.Command {
+func GetCmdDistributeReward(cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
-		Use:   "sign <file>",
-		Short: "sign <file>",
-		Args:  cobra.ExactArgs(1),
+		Use:   "distribute-reward <receiver> <id> <reward>",
+		Short: "distribute-reward",
+		Aliases: []string{"dr"},
+		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
-
 			inBuf := bufio.NewReader(cmd.InOrStdin())
-			kb, err := keyring.NewKeyring(sdk.KeyringServiceName(), viper.GetString(flags.FlagKeyringBackend), viper.GetString(flags.FlagHome), inBuf)
+			txBldr := auth.NewTxBuilderFromCLI(inBuf).WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			receiver, err := sdk.AccAddressFromBech32(args[0])
 			if err != nil {
-				return fmt.Errorf("failed to get keyring: %w", err)
+				return fmt.Errorf("failed to parse receiver: %w", err)
 			}
 
-			pdv, err := ioutil.ReadFile(args[0])
+			id, err := strconv.ParseUint(args[1], 10, 64)
 			if err != nil {
-				return fmt.Errorf("failed to read pdv file: %w", err)
-			}
-			msg := append(pdv, []byte("/v1/pdv")...)
-
-			signature, pk, err := kb.Sign(cliCtx.GetFromName(), keys.DefaultKeyPass, msg)
-			if err != nil {
-				return fmt.Errorf("failed to sign: %w", err)
+				return fmt.Errorf("failed to parse id: %w", err)
 			}
 
-			return cliCtx.PrintOutput(struct {
-				PublicKey string `json:"pubic_key"`
-				Signature string `json:"signature"`
-			}{
-				PublicKey: hex.EncodeToString(pk.Bytes()[5:]), // cut amino codec prefix
-				Signature: hex.EncodeToString(signature),
-			})
+			reward, err := strconv.ParseUint(args[2], 10, 64)
+			if err != nil {
+				return fmt.Errorf("failed to parse reward: %w", err)
+			}
+
+			msg := types.NewMsgDistributeRewards(cliCtx.FromAddress, []types.Reward{{
+				Receiver: receiver,
+				ID:       id,
+				Reward:   reward,
+			}})
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
 		},
 	}
 }
