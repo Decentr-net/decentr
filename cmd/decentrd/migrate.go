4e6b41ec6ed548f2e0230ed7e13da9e61a310b51
Vladislav Mikitich <vmikitich@gmail.com>
2021-11-19 02:57:21 +0300

    migrate genesis cmd (#84)

---
+++ cmd/decentrd/migrate.go
@@ -0,0 +1,139 @@
+package main
+
+// this file is adapted version of github.com/cosmos/cosmos-sdk/x/genutil/client/cli/migrate.go
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	tmjson "github.com/tendermint/tendermint/libs/json"
+	tmtypes "github.com/tendermint/tendermint/types"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/version"
+	"github.com/cosmos/cosmos-sdk/x/genutil/types"
+
+	v150 "github.com/Decentr-net/decentr/migrate/v150"
+)
+
+const flagGenesisTime = "genesis-time"
+
+const chainUpgradeGuide = "https://docs.cosmos.network/master/migrations/chain-upgrade-guide-040.html"
+
+var migrationMap = types.MigrationMap{
+	"v1.5.0": v150.Migrate,
+}
+
+// GetMigrationCallback returns a MigrationCallback for a given version.
+func GetMigrationCallback(version string) types.MigrationCallback {
+	return migrationMap[version]
+}
+
+// MigrateGenesisCmd returns a command to execute genesis state migration.
+func MigrateGenesisCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "migrate [target-version] [genesis-file]",
+		Short: "Migrate genesis to a specified target version",
+		Long: fmt.Sprintf(`Migrate the source genesis into the target version and print to STDOUT.
+
+Example:
+$ %s migrate v1.5.0 /path/to/genesis.json --chain-id=mainnet-2 --genesis-time=2021-11-22T17:00:00Z
+`, version.AppName),
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			var err error
+
+			target := args[0]
+			importGenesis := args[1]
+
+			genDoc, err := validateGenDoc(importGenesis)
+			if err != nil {
+				return err
+			}
+
+			// Since some default values are valid values, we just print to
+			// make sure the user didn't forget to update these values.
+			if genDoc.ConsensusParams.Evidence.MaxBytes == 0 {
+				fmt.Printf("Warning: consensus_params.evidence.max_bytes is set to 0. If this is"+
+					" deliberate, feel free to ignore this warning. If not, please have a look at the chain"+
+					" upgrade guide at %s.\n", chainUpgradeGuide)
+			}
+
+			var initialState types.AppMap
+			if err := json.Unmarshal(genDoc.AppState, &initialState); err != nil {
+				return errors.Wrap(err, "failed to JSON unmarshal initial genesis state")
+			}
+
+			migrationFunc := GetMigrationCallback(target)
+			if migrationFunc == nil {
+				return fmt.Errorf("unknown migration function for version: %s", target)
+			}
+
+			// TODO: handler error from migrationFunc call
+			newGenState := migrationFunc(initialState, clientCtx)
+
+			genDoc.AppState, err = json.Marshal(newGenState)
+			if err != nil {
+				return errors.Wrap(err, "failed to JSON marshal migrated genesis state")
+			}
+
+			genesisTime, _ := cmd.Flags().GetString(flagGenesisTime) // nolint
+			if genesisTime != "" {
+				var t time.Time
+
+				err := t.UnmarshalText([]byte(genesisTime))
+				if err != nil {
+					return errors.Wrap(err, "failed to unmarshal genesis time")
+				}
+
+				genDoc.GenesisTime = t
+			}
+
+			chainID, _ := cmd.Flags().GetString(flags.FlagChainID) // nolint
+			if chainID != "" {
+				genDoc.ChainID = chainID
+			}
+
+			bz, err := tmjson.Marshal(genDoc)
+			if err != nil {
+				return errors.Wrap(err, "failed to marshal genesis doc")
+			}
+
+			sortedBz, err := sdk.SortJSON(bz)
+			if err != nil {
+				return errors.Wrap(err, "failed to sort JSON genesis doc")
+			}
+
+			cmd.Println(string(sortedBz))
+			return nil
+		},
+	}
+
+	cmd.Flags().String(flagGenesisTime, "", "override genesis_time with this flag")
+	cmd.Flags().String(flags.FlagChainID, "", "override chain_id with this flag")
+
+	return cmd
+}
+
+// validateGenDoc reads a genesis file and validates that it is a correct
+// Tendermint GenesisDoc. This function does not do any cosmos-related
+// validation.
+func validateGenDoc(importGenesisFile string) (*tmtypes.GenesisDoc, error) {
+	genDoc, err := tmtypes.GenesisDocFromFile(importGenesisFile)
+	if err != nil {
+		return nil, fmt.Errorf("%s. Make sure that"+
+			" you have correctly migrated all Tendermint consensus params, please see the"+
+			" chain migration guide at %s for more info",
+			err.Error(), chainUpgradeGuide,
+		)
+	}
+
+	return genDoc, nil
+}
