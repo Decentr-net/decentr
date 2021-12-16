f2478cb43effc76bc680360912e123076865bba9
Vladislav Mikitich <vmikitich@gmail.com>
2021-12-16 22:57:10 +0300

    Fixes (#87)

--- migrate/v150/cosmos.go
+++ migrate/v150/cosmos.go
@@ -2,18 +2,38 @@ package v150
 
 import (
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	evidencetypes "github.com/cosmos/cosmos-sdk/x/evidence/types"
 	v040genutil "github.com/cosmos/cosmos-sdk/x/genutil/legacy/v040"
 	v043genutil "github.com/cosmos/cosmos-sdk/x/genutil/legacy/v043"
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
+	v039slashing "github.com/cosmos/cosmos-sdk/x/slashing/legacy/v039"
 	ibctransfertypes "github.com/cosmos/ibc-go/modules/apps/transfer/types"
 	ibchost "github.com/cosmos/ibc-go/modules/core/24-host"
 	"github.com/cosmos/ibc-go/modules/core/exported"
 	ibctypes "github.com/cosmos/ibc-go/modules/core/types"
 )
 
+func patchSlashingParams(appState genutiltypes.AppMap) genutiltypes.AppMap {
+	v039Codec := codec.NewLegacyAmino()
+
+	if appState[v039slashing.ModuleName] != nil {
+		// unmarshal relative source genesis application state
+		var slashingGenState v039slashing.GenesisState
+		v039Codec.MustUnmarshalJSON(appState[v039slashing.ModuleName], &slashingGenState)
+
+		slashingGenState.Params.SignedBlocksWindow = 600
+
+		appState[v039slashing.ModuleName] = v039Codec.MustMarshalJSON(slashingGenState)
+	}
+
+	return appState
+}
+
 func migrateCosmosAppState(appState genutiltypes.AppMap, clientCtx client.Context) genutiltypes.AppMap {
+	appState = patchSlashingParams(appState)
+
 	appState = v040genutil.Migrate(appState, clientCtx)
 	appState = v043genutil.Migrate(appState, clientCtx)
 
