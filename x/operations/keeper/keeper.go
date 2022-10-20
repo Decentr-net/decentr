3fca68bfd84f9ae8f569579c21ca6e0f618fae70
Andrei Schneider <megaherz@bk.ru>
2022-10-20 09:41:43 +0300

    fix block seq (#97)

--- x/operations/keeper/keeper.go
+++ x/operations/keeper/keeper.go
@@ -3,12 +3,15 @@ package keeper
 import (
 	"fmt"
 
+	"github.com/Decentr-net/decentr/config"
+
 	"github.com/tendermint/tendermint/libs/log"
 
-	"github.com/Decentr-net/decentr/x/operations/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+
+	"github.com/Decentr-net/decentr/x/operations/types"
 )
 
 type Keeper struct {
@@ -46,6 +49,14 @@ func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 	k.paramSpace.GetParamSet(ctx, &params)
 	return
+}
+
+func (k Keeper) GetMinGasPrice(ctx sdk.Context) sdk.DecCoin {
+	params := types.Params{
+		MinGasPrice: sdk.NewDecCoin(config.DefaultBondDenom, sdk.ZeroInt()),
+	}
+	k.paramSpace.GetParamSetIfExists(ctx, &params)
+	return params.MinGasPrice
 }
 
 func (k Keeper) IsSupervisor(ctx sdk.Context, address sdk.AccAddress) bool {
